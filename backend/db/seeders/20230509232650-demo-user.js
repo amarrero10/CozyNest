4ecd066bfba22f5f2dd5db58c4930a2471dd1590
Albert Marrero <albert.marrero10@gmail.com>
2023-05-13 14:21:32 -0400

    Re-render

--- backend/db/seeders/20230509232650-demo-user.js
+++ backend/db/seeders/20230509232650-demo-user.js
@@ -16,16 +16,22 @@ module.exports = {
           email: "demo@user.io",
           username: "Demo-lition",
           hashedPassword: bcrypt.hashSync("password"),
+          firstName: "demo",
+          lastName: "user",
         },
         {
           email: "user1@user.io",
           username: "FakeUser1",
           hashedPassword: bcrypt.hashSync("password2"),
+          firstName: "user",
+          lastName: "Onee",
         },
         {
           email: "user2@user.io",
           username: "FakeUser2",
           hashedPassword: bcrypt.hashSync("password3"),
+          firstName: "usertwo",
+          lastName: "twoo",
         },
       ],
       {}
