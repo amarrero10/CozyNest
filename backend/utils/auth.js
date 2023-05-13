2653a69c418c61fbc192908ae67e71882cc11eb4
Albert Marrero <albert.marrero10@gmail.com>
2023-05-13 15:19:49 -0400

    Re-Render

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -11,6 +11,8 @@ const setTokenCookie = (res, user) => {
     id: user.id,
     email: user.email,
     username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
   };
   const token = jwt.sign(
     { data: safeUser },
