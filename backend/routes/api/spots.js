b012600f73105f667397052b489e80d55075dcd7
Albert Marrero <albert.marrero10@gmail.com>
2023-05-17 19:14:18 -0400

    Working on Spot Feature

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,9 +4,41 @@ const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User, Spot } = require("../../db/models");
+const { User, Spot, Image } = require("../../db/models");
 
 const router = express.Router();
+
+const validateSpot = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required.")
+    .isLength({ min: 5 })
+    .withMessage("Street address is not valid"),
+  check("city").exists({ checkFalsy: true }).isLength({ min: 3 }).withMessage("City is required"),
+  check("state")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("State is required."),
+  check("country")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 3 })
+    .withMessage("Country is required"),
+  check("lat").exists({ checkFalsy: true }).isFloat().withMessage("Latitude is not valid"),
+  check("lon").exists({ checkFalsy: true }).isFloat().withMessage("Longitude is not valid."),
+  check("name")
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50 })
+    .withMessage("Name must be less than 50 characters"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1, max: 1500 })
+    .withMessage("Description is required"),
+  check("price")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .withMessage("Price per day is required"),
+  handleValidationErrors,
+];
 
 // Get Spot by Id
 router.get("/:id", async (req, res) => {
@@ -47,6 +79,26 @@ router.get("/", async (req, res) => {
   }
 
   return res.json(spots);
+});
+
+// Add new spot
+router.post("/", validateSpot, requireAuth, async (req, res) => {
+  const { address, city, state, country, lat, lon, name, description, price } = req.body;
+
+  const newSpot = await Spot.create({
+    ownerId: req.user.id,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lon,
+    name,
+    description,
+    price,
+  });
+
+  res.status(201).json(newSpot);
 });
 
 module.exports = router;
