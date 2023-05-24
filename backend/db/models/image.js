6b7c398af80b9763b78e622e7cb7eda5dca31ccb
Albert Marrero <albert.marrero10@gmail.com>
2023-05-24 19:38:28 -0400

    API Test Routes

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
         scope: {
           imageableType: "spot",
         },
+        as: "SpotImages",
       });
 
       Image.belongsTo(models.Review, {
